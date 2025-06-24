6d61385c441fd554081c8da492fc9bc5e89e32e4
ck496 <chriskurian24@gmail.com>
2025-06-24 02:41:23 -0700

    refactor: clean up navbar button creation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,15 +5,14 @@ import ColorModeToggle from "./ColorModeToggle";
 import NavBarButtons from "./NavBarButtons";
 
 const Navbar = () => {
+  const navBarTitles = ["Home", "Notes", "Tools", "Leaderboards", "About"];
   return (
     <Flex as="nav" paddingX={4} paddingY={2} gap={0}>
       <HStack gap={1} align="center">
         <Image src={logo} boxSize="50px"></Image>
-        <NavBarButtons>Home</NavBarButtons>
-        <NavBarButtons>Notes</NavBarButtons>
-        <NavBarButtons>Tools</NavBarButtons>
-        <NavBarButtons>Leaderboards</NavBarButtons>
-        <NavBarButtons>About</NavBarButtons>
+        {navBarTitles.map((title) => (
+          <NavBarButtons key={title}>{title}</NavBarButtons>
+        ))}
       </HStack>
       <Spacer />
       <HStack gap={4}>
