8d7c618d8dddc70cd109382ef1fad8fed10a5442
ck496 <chriskurian24@gmail.com>
2025-06-23 23:53:30 -0700

    feat(home-layout): show/hide side bar based on screen size

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import "./App.css";
-import { Button, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Grid, GridItem, Show, Text } from "@chakra-ui/react";
 
 function App() {
-
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main" "bottom-list"`,
-        lg: `"nav nav"  "main side" "bottom-list bottom-list"`, //For devices larger than 1020 px show side bar etc
+        base: `"nav" "main" "bottom"`,
+        lg: `"nav nav"  "main side" "bottom bottom"`, //For devices larger than 1020 px show side bar etc
       }}
       templateColumns={{
         base: "1fr",
-        lg: "2fr 1fr ",
+        lg: "3fr 1fr",
       }}
     >
       <GridItem area="nav" bg="tomato">
@@ -20,10 +19,12 @@ function App() {
       <GridItem area="main" bg="papayawhip">
         Main
       </GridItem>
-      <GridItem area="side" bg="gold">
+
+      <GridItem area="side" bg="gold" display={{ base: "none", lg: "block" }}>
         Side
       </GridItem>
-      <GridItem area="bottom-list" bg="yellow">
+
+      <GridItem area="bottom" bg="yellow">
         Bottom List
       </GridItem>
     </Grid>
