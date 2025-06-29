84a8badbffaac6a80053f55403c924adb6e06fa7
ck496 <chriskurian24@gmail.com>
2025-06-29 02:11:12 -0700

    ux: added hover feature on buttons

--- src/components/ColorModeToggle.tsx
+++ src/components/ColorModeToggle.tsx
@@ -1,8 +1,21 @@
-import { ColorModeButton, useColorMode } from "./ui/color-mode";
+import {
+  ColorModeButton,
+  useColorMode,
+  useColorModeValue,
+} from "./ui/color-mode";
 
 const ColorModeToggle = () => {
   const { toggleColorMode } = useColorMode();
-  return <ColorModeButton onClick={toggleColorMode} />;
+  return (
+    <ColorModeButton
+      onClick={toggleColorMode}
+      cursor="pointer"
+      _hover={{
+        bg: useColorModeValue("#9171dc", "#45248f"),
+        color: "white",
+      }}
+    />
+  );
 };
 
 export default ColorModeToggle;
--- src/components/NavBarButtons.tsx
+++ src/components/NavBarButtons.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonProps } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { NavButtonValues } from "../data/navButtonValues";
+import { useColorModeValue } from "./ui/color-mode";
 
 interface NavButtonProps extends ButtonProps {
   buttonValue: NavButtonValues;
@@ -8,7 +9,18 @@ interface NavButtonProps extends ButtonProps {
 const NavBarButtons = ({ buttonValue, ...props }: NavButtonProps) => {
   return (
     <Link key={buttonValue.path} to={buttonValue.path}>
-      <Button variant="ghost" size="sm" paddingX={2} minWidth="auto" {...props}>
+      <Button
+        variant="ghost"
+        size="lg"
+        paddingX={2}
+        minWidth="auto"
+        cursor="pointer"
+        _hover={{
+          bg: useColorModeValue("#9171dc", "#45248f"),
+          color: "white",
+        }}
+        {...props}
+      >
         {buttonValue.label}
       </Button>
     </Link>
