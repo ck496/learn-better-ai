0e0a5dd301cd22744fd888188fc8c21f2bf2586a
ck496 <chriskurian24@gmail.com>
2025-06-29 01:53:04 -0700

    feat: updated HeroSection with new background image and better ux

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,33 +10,51 @@ import {
   Image,
   HStack,
 } from "@chakra-ui/react";
-
-import { useColorModeValue } from "./ui/color-mode";
+import bannerImg from "../assets/banner/grainBanner.webp";
 
 const HeroSection = () => {
-  const bg = useColorModeValue("gray.300", "gray.800");
-  const textColor = useColorModeValue("gray.900", "white");
-
+  // const bg = useColorModeValue("gray.300", "gray.800");
   return (
-    <Box as="section" bg={bg} py={{ base: 12, md: 24 }} h="100%">
+    <Box
+      as="section"
+      minH={{ base: "auto", md: "70vh" }}
+      py={{ base: 12, md: 24 }}
+      h="100%"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      backgroundImage={`url(${bannerImg})`}
+      backgroundColor="blackAlpha.200"
+      backgroundBlendMode="overlay"
+    >
       <Container maxW="container.lg">
         <Flex
           direction={{ base: "column", md: "row" }}
-          align="center"
-          justify="space-between"
+          align="flex-start" // align items to the top of the row
+          justify="flex-start" // push everything to the left
           gap={{ base: 8, md: 16 }}
           padding={4}
         >
           {/* Text Column */}
-          <Box flex="1">
-            <Heading as="h1" size="2xl" color={textColor} mb={4}>
-              AI-Powered Tech Guides
+          <Box
+            textAlign="left"
+            width="70%"
+            flex="0 0 40%" // never grow, never shrink, always 30% wide
+            minW="0" // allow it to shrink below its content’s min-width
+          >
+            <Heading as="h1" size="5xl" color="gray.100" mb={4}>
+              Learn-Better.AI
             </Heading>
-            <Text fontSize="lg" color={textColor} mb={6}>
-              Instantly convert your Markdown notes into engaging, expert-level
-              blog posts—tailored to any reader’s expertise.
+            <Text fontSize="2xl" color="gray.100" mb={2}>
+              Escape tutorial hell and build faster
             </Text>
-            <HStack justify="center" gap={5} p={4}>
+            <Text fontSize="lg" color="gray.100" mb={6}>
+              Transform years of scattered notes into precise, adaptive, and
+              searchable tech guides—foster community knowledge sharing and
+              slash onboarding time by 70%
+            </Text>
+
+            <HStack justify="left" gap={5} p={4}>
               <Button colorScheme="blue" size="lg" borderRadius={50}>
                 Get Started
               </Button>
